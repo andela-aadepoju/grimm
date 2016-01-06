f469c24d157b92ed19c85ff98322f7d33b507dd5
mradeybee <mradeybee@gmail.com>
2016-01-06 04:29:17 +0100

    add controller handler in preparation for routing

--- lib/grimm.rb
+++ lib/grimm.rb
@@ -3,7 +3,22 @@ require "grimm/version"
 module Grimm
   class Application
     def call(env)
-      [200, {"Content-Type" => "text/html"}, ["Hello"]]
+      if env["PATH_INFO"]== "/"
+        return [302, {"Location" => "/pages/index"}, []]
+      end
+      if env["PATH_INFO"]== "/favicon.ico"
+        return [400, {}, []]
+      end
+
+      controller_class, action = get_controller_and_action(env)
+      response = controller_class.new.send(action)
+      [200, { "Content-Type" => "text/html" }, [response]]
+    end
+
+    def get_controller_and_action(env)
+      _, controller_name, action = env["PATH_INFO"].split("/")
+      controller_name = controller_name.capitalize + "Controller"
+      [Object.const_get(controller_name), action]
     end
   end
 end
