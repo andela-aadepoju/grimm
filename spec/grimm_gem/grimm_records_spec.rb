626721412a8d8dc655b8d7978aa7a6e28e4a71f4
Adebayo <mradeybee@gmail.com>
2016-01-26 00:21:59 +0100

    resolve other style guide violations

--- spec/grimm_gem/grimm_records_spec.rb
+++ spec/grimm_gem/grimm_records_spec.rb
@@ -1,8 +1,9 @@
 require "spec_helper"
 describe "grimm record" do
   describe "#create_table_query" do
+    auto = ["AUTOINCREMENT"]
     it "returns creat query hash as string" do
-      expect(List.get_table_query([], "autoincrement", "ture")).to eq ["AUTOINCREMENT"]
+      expect(List.get_table_query([], "autoincrement", "ture")).to eq auto
     end
   end
 end
